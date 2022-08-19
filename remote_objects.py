28679ed3a134b44935147237d962493c9911415a
Cautisanu <accautisanu@luxoft.com>
2022-08-19 15:33:43 +0300

    Add post func and reqs.py removed
    Added post function inside the root class, individual instances of subclasses can now pe posted by doing <name>.post()

--- remote_objects.py
+++ remote_objects.py
@@ -33,7 +33,14 @@ class RemoteObject:
 
 
     def post(self):
-        pass
+        headers = {
+            'Authorization': 'Bearer a81345463eb65f45373d18174a0bf2750c85c6bbc2f4d4f687981c426ce0d47d'
+        }
+
+        url = 'https://gorest.co.in/public/v2' + self.url
+        response = requests.post(url, headers=headers, data=self.__dict__)
+
+        return response 
 
 
 class User(RemoteObject):
--- testing.py
+++ testing.py
@@ -1,14 +1,25 @@
 from remote_objects import RemoteObject, User, Post, Comment, Todo
 import requests
 
-base_url = 'https://gorest.co.in/public/v2/'
-user = User()
-user.get_by_id(2783)
-print(user.name)
+# base_url = 'https://gorest.co.in/public/v2/'
+# user = User()
+# user.get_by_id(2783)
+# print(user.name)
 
-print(user.get_all())
+# print(user.get_all())
 
 
-post = Post()
-print(Post.get_all(user_id=3566))
-print(post)
+# mister_scholz = User()
+# mister_scholz.get_by_id(3542)
+# mister_scholz.name = 'Olaf Scholz'
+# mister_scholz.status = 'active'
+# mister_scholz.email = 'the_actual_chancellor@live.com'
+
+# print(mister_scholz.post().json())
+
+# response = requests.delete('https://gorest.co.in/public/v2/users/5539', headers={
+#             'Authorization': 'Bearer a81345463eb65f45373d18174a0bf2750c85c6bbc2f4d4f687981c426ce0d47d'
+#         })
+
+# print(response)
+# print(response.text)
