fed609c156814cd037a8119eedc26c84ff0b654a
Cautisanu <accautisanu@luxoft.com>
2022-09-13 15:13:09 +0300

    TOML recursiveness
    Can now have children defined inside the TOML file

--- toml_methods.py
+++ toml_methods.py
@@ -5,14 +5,32 @@ import backend_functions as bf
 from vars import base_url, type_url, headers
 
 
+def post_with_children(url, parent_obj, parent_obj_type):
+    status_code, payload = bf.POST(url, headers, parent_obj)
+    print(payload)
+    
+    if status_code == 201:
+        id = payload['id']
+        for key in ['user', 'post', 'comment', 'todo']:
+            if key in parent_obj:
+                for obj in parent_obj[key]:
+                    obj[f'{parent_obj_type}_id'] = id
+                    post_with_children(type_url[key], obj, key)
+    
+    else:
+        print(f'ERROR STATUS CODE {status_code}')
+
+
 def post_from_toml(file_path: str):
     content = toml.load(file_path)
     for key in ['user', 'post', 'comment', 'todo']:
         if key in content:
             for _key in content[key]:
-                status_code, payload = bf.POST(type_url[key], headers, content[key][_key])
-
+                post_with_children(type_url[key], content[key][_key], key)
 
 if __name__ == '__main__':
 
     post_from_toml(r'objects\objects_to_post.toml')
+
+    #content = toml.load(r'objects\objects_to_post.toml')
+    #print(content['user'])
